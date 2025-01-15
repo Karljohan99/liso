4665d3f71d79f5362d8e97883af6db61bb510d72
Karl-Johan Pilve <karljohan30@gmail.com>
2025-01-15 16:41:10 +0200

    Update singularity paths

--- prepare_tartu_data.bash
+++ prepare_tartu_data.bash
@@ -8,4 +8,4 @@ source $HOME/liso/install_extra_packages.bash
 
 conda init && source $HOME/.bashrc && conda activate liso
 
-python $HOME/liso/liso/datasets/tartu/create_tartu_raw.py --tartu_raw_root /gpfs/space/projects/ml2024 --target_dir /gpfs/space/projects/Bolt/liso/tartu_dataset --pcd_locations /gpfs/space/projects/Bolt/liso/tartu_pcd_locations
+python $HOME/liso/liso/datasets/tartu/create_tartu_raw.py --tartu_raw_root /mnt/ml2024 --target_dir /mnt/Bolt/liso/tartu_dataset --pcd_locations /gpfs/Bolt/liso/tartu_pcd_locations
