f8f3ed60710b6dc3f17494cb400f7df59b0ddd8c
Karl-Johan Pilve <karljohan30@gmail.com>
2025-01-17 16:33:26 +0200

    Tartu SLIM training scripts

--- train_slim_tartu.bash
+++ train_slim_tartu.bash
@@ -2,10 +2,6 @@
 
 export LD_LIBRARY_PATH="/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
 
-ldd --version
-
-source $HOME/liso/install_extra_packages.bash
-
 conda init && source $HOME/.bashrc && conda activate liso
 
-python3 liso/slim/cli.py --summary-dir $HOME/liso/data/train_slim_tartu -c slim_simple_knn_training slim_tartu use_lidar_intensity slim_RAFT batch_size_one slim_highest_resolution
+python3 liso/slim/cli.py --summary-dir /mnt/Bolt/train_logs -c slim_simple_knn_training slim_tartu use_lidar_intensity slim_RAFT batch_size_one slim_highest_resolution
