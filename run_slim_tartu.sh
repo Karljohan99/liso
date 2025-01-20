1202e9ebf9ba67e4a9eb8ee830797963d878c08b
Karl-Johan Pilve <karljohan30@gmail.com>
2025-01-20 15:16:28 +0200

    Update name

--- run_slim_tartu.sh
+++ run_slim_tartu.sh
@@ -30,4 +30,4 @@
 
 module load singularity
 
-singularity run --nv --bind /gpfs/space/projects:/mnt ${HOME}/liso/liso_dev.sif ${HOME}/liso/train_slim_tartu.bash
+singularity run --nv --bind /gpfs/space/projects:/mnt ${HOME}/liso/liso_tartu.sif ${HOME}/liso/train_slim_tartu.bash
