f513c1ab4c9f92a8f36d4728d64d4971b5cc3cf5
Karl-Johan Pilve <karljohan30@gmail.com>
2025-01-20 15:12:50 +0200

    Update path

--- train_slim_tartu.bash
+++ train_slim_tartu.bash
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH="/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
-
 conda init && source $HOME/.bashrc && conda activate liso
 
-python3 liso/slim/cli.py --summary-dir /mnt/Bolt/train_logs -c slim_simple_knn_training slim_tartu use_lidar_intensity slim_RAFT batch_size_one slim_highest_resolution
+python3 liso/slim/cli.py --summary-dir /mnt/Bolt/liso/train_logs -c slim_simple_knn_training slim_tartu use_lidar_intensity slim_RAFT batch_size_one slim_highest_resolution
