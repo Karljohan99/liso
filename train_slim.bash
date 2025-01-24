b6a1592dd8354666df81464d526a5866615cef1b
Karl-Johan Pilve <karljohan30@gmail.com>
2025-01-24 10:31:39 +0200

    Use sota SLIM

--- train_slim.bash
+++ train_slim.bash
@@ -2,4 +2,4 @@
 
 conda init && source $HOME/.bashrc && conda activate liso
 
-python3 liso/slim/cli.py --summary-dir /mnt/Bolt/liso/kitti_train_logs -c slim_simple_knn_training slim_kitti use_lidar_intensity slim_RAFT batch_size_one slim_highest_resolution
+python3 liso/slim/cli.py --summary-dir /mnt/Bolt/liso/kitti_train_logs -c slim_sota_us slim_kitti use_lidar_intensity slim_RAFT batch_size_one slim_highest_resolution
