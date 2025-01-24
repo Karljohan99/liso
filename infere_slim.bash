98eb7a3efb430eb878a015a34a681879c75ca93a
Karl-Johan Pilve <karljohan30@gmail.com>
2025-01-24 09:32:42 +0200

    Fixes

--- infere_slim.bash
+++ infere_slim.bash
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH="/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
-
-ldd --version
-
-# source $HOME/liso/install_extra_packages.bash
-
 conda init && source $HOME/.bashrc && conda activate liso
 
-python3 liso/slim/cli.py --inference-only --summary-dir $HOME/liso/data/train_slim/first_inference --load_checkpoint $HOME/liso/data/train_slim/first_train/b177b/20250110_155802/checkpoints/15000.pth
+python3 liso/slim/cli.py --inference-only --summary-dir /mnt/Bolt/liso/kitti_slim_infere_logs --load_checkpoint Bolt/liso/kitti_train_logs/f39a0/20250123_150226/checkpoints/15000.pth
 
 
