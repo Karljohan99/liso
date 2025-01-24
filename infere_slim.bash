3f91ad30433acc67cc0bdf3425b309076cb2a6ed
Karl-Johan Pilve <karljohan30@gmail.com>
2025-01-24 11:47:19 +0200

    Update

--- infere_slim.bash
+++ infere_slim.bash
@@ -2,6 +2,6 @@
 
 conda init && source $HOME/.bashrc && conda activate liso
 
-python3 liso/slim/cli.py --inference-only --summary-dir /mnt/Bolt/liso/kitti_slim_infere_logs --load_checkpoint /mnt/Bolt/liso/kitti_train_logs/f39a0/20250123_150226/checkpoints/15000.pth
+python3 liso/slim/cli.py --inference-only --summary-dir /mnt/Bolt/liso/kitti_slim_infere_logs --load_checkpoint /mnt/Bolt/liso/kitti_train_logs/4b3c3/20250124_103751/checkpoints/500.pth
 
 
