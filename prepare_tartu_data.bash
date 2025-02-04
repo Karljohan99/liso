84ed9ffacf4f63ac2cad4fe678831f03e8bbeaa0
Karl-Johan Pilve <karljohan30@gmail.com>
2025-02-04 12:50:28 +0200

    Update script

--- prepare_tartu_data.bash
+++ prepare_tartu_data.bash
@@ -2,4 +2,4 @@
 
 conda init && source $HOME/.bashrc && conda activate liso
 
-python liso/dataset/tartu/create_tartu_filtered.py --target_dir /mnt/Bolt/liso/tartu_dataset --filtered_data_file data.txt --tartu_raw_root /mnt/ml2024 --pcd_locations /mnt/Bolt/liso/tartu_pcd_locations
+python liso/datasets/tartu/create_tartu_filtered.py --target_dir /mnt/Bolt/liso/tartu_dataset --filtered_data_file liso/datasets/tartu/data.txt --tartu_raw_root /mnt/ml2024 --pcd_locations /mnt/Bolt/liso/tartu_pcd_locations
