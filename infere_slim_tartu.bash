47863f3859e08878ec604415e9d2aa12561ab046
Karl-Johan Pilve <karljohan30@gmail.com>
2025-02-05 09:28:00 +0200

    New SLIM interference

--- infere_slim_tartu.bash
+++ infere_slim_tartu.bash
@@ -2,6 +2,6 @@
 
 conda init && source $HOME/.bashrc && conda activate liso
 
-python3 liso/slim/cli.py --inference-only --summary-dir /mnt/Bolt/liso/slim_infere_logs --load_checkpoint /mnt/Bolt/liso/train_logs/d0cf1/20250125_185514/checkpoints/150000.pth
+python3 liso/slim/cli.py --inference-only --summary-dir /mnt/Bolt/liso/slim_infere_logs --load_checkpoint /mnt/Bolt/liso/train_logs/70295/20250204_152838/checkpoints/16000.pth
 
 
