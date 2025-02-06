e1bffb54241c8f541aa8843ed2bda401305d2af7
Karl-Johan Pilve <karljohan30@gmail.com>
2025-02-06 14:03:07 +0200

    LISO tartu scripts

--- train_liso_tartu.bash
+++ train_liso_tartu.bash
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH="/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
-
-ldd --version
-
-# source $HOME/liso/install_extra_packages.bash
-
 conda init && source $HOME/.bashrc && conda activate liso
 
-python3 liso/kabsch/liso_cli.py --summary-dir $/mnt/Bolt/train_logs -c nuscenes bev_100m_512 centerpoint batch_size_four liso
+python3 liso/kabsch/liso_cli.py --summary-dir $/mnt/Bolt/liso/train_logs -c tartu bev_100m_512 centerpoint batch_size_four liso
