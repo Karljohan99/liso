dfff446bfd1d2c7ad68b0d5328af1e0ee15d7fc8
Karl-Johan Pilve <karljohan30@gmail.com>
2025-02-11 09:54:22 +0200

    Update script

--- export_scene_flow_tartu.sh
+++ export_scene_flow_tartu.sh
@@ -15,6 +15,12 @@
 #SBATCH -t 8-00:00:00
 #SBATCH --mem=16G
 
+# Keep this line if you need a GPU for your job
+#SBATCH --partition=gpu
+
+# Indicates that you need one GPU node
+#SBATCH --gres=gpu:tesla:1
+
 # If you keep the next two lines, you will get an e-mail notification
 # whenever something happens to your job (it starts running, completes or fails)
 #SBATCH --mail-type=ALL
