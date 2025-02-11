0f26ca2dfd59731f3c0a0fde5d12812ad50ca416
Karl-Johan Pilve <karljohan30@gmail.com>
2025-02-11 09:07:18 +0200

    Update path

--- infere_slim_tartu.bash
+++ infere_slim_tartu.bash
@@ -2,6 +2,6 @@
 
 conda init && source $HOME/.bashrc && conda activate liso
 
-python3 liso/slim/cli.py --inference-only --summary-dir /mnt/Bolt/liso/slim_infere_logs --load_checkpoint /mnt/Bolt/liso/train_logs/70295/20250205_234012/checkpoints/50000.pth
+python3 liso/slim/cli.py --inference-only --summary-dir /mnt/Bolt/liso/slim_infere_logs --load_checkpoint /mnt/Bolt/liso/train_logs/7695f/20250207_223658/checkpoints/150000.pth
 
 
